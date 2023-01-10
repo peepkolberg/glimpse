0b07e5fbce7f6622c985ce52ab959b23774c8f60
Peep Kolberg <peepkolberg@gmail.com>
2023-01-10 20:45:28 +0200

    Remove unnecessary modules. Small adjustments

--- run.sh
+++ run.sh
@@ -5,12 +5,10 @@
 #SBATCH --time=1-00:00:00
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=1
-#SBATCH --mem=4G
+#SBATCH --mem=2G
 
 
 
-module load any/jdk/1.8.0_265
-module load any/singularity/3.5.3
 module load nextflow
 module load any/bcftools
 module load any/glimpse/1.1.1
@@ -19,8 +17,8 @@ module load any/glimpse/1.1.1
 # chromosomes is a comma-separated list of autosomal chromosomes. Sex chromosomes will probably break the code.
 # Reference panel and genome expect index files to be present in the same directories as the references. Index files must have the same name as reference file + ".csi" for reference panel and + ".fai" for reference genome. These are hard-coded in main.nf workflow{} section.
 # genetic_map points to a directory. In the directory, expects a separate file for each chromosome. The filenames must follow this convention: chr{i}.gmap.gz, where {i} is replaced by chromosome number.
-nextflow run main.nf -profile tartu_hpc \
-    --samples data/samples.tsv \
+nextflow -log logs run main.nf -profile tartu_hpc \
+    --samples /path/to/samples.tsv \
     --chromosomes 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22 \
     --reference_panel /path/to/reference_panel.vcf.gz \
     --reference_genome /path/to/reference_genome.fa \
