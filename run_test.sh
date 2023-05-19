6762f5bf1be6c298887c8e5bc607329f5c728389
peepkolberg <peepkolberg@gmail.com>
2023-05-19 09:33:40 +0300

    Add example of running test

---
+++ run_test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --job-name=glimpse
+#SBATCH --partition=amd
+#SBATCH --time=1-00:00:00
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=1G
+
+
+
+module load nextflow/22.04.3
+module load any/singularity/3.11.1
+
+nextflow -log logs/.nextflow.log run main.nf -profile tartu_hpc_test
