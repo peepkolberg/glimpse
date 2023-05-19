9b9cdf282cf5c82a77a28c64a61926ff1f409b84
peepkolberg <peepkolberg@gmail.com>
2023-05-19 09:36:59 +0300

    Minor refactor

--- run.sh
+++ run.sh
@@ -12,9 +12,9 @@
 module load nextflow/22.04.3
 module load any/singularity/3.11.1
 
-# Maybe sample bam files need to be sorted. An example is given in data/samples.tsv. Each sample expects an index file to be present in the same directory as the bam file. Index file must have the same name as bam file + ".bai". This is hard-coded in main.nf workflow{} section.
+# Maybe sample bam files need to be sorted. An example is given in data/samples.tsv. Each sample expects an index file to be present in the same directory as the bam file. Index file must have the same name as bam file + ".bai".
 # chromosomes is a comma-separated list of autosomal chromosomes. Sex chromosomes will probably break the code.
-# Reference panel and genome expect index files to be present in the same directories as the references. Index files must have the same name as reference file + ".csi" for reference panel and + ".fai" for reference genome. These are hard-coded in main.nf workflow{} section.
+# Reference panel and genome expect index files to be present in the same directories as the references. Index files must have the same name as reference file + ".csi" for reference panel and + ".fai" for reference genome.
 # genetic_map points to a directory. In the directory, expects a separate file for each chromosome. The filenames must follow this convention: chr{i}.gmap.gz, where {i} is replaced by chromosome number.
 nextflow -log logs/.nextflow.log run main.nf -profile tartu_hpc \
     --samples /path/to/samples.tsv \
