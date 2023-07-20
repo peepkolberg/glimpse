eef31ab15c57c6af3f7295eb26b7166d05594be7
peepkolberg <peepkolberg@gmail.com>
2023-07-20 18:51:16 +0300

    Specify outdir param

--- run_test.sh
+++ run_test.sh
@@ -10,4 +10,6 @@
 module load nextflow/22.04.3
 module load any/singularity/3.11.1
 
-nextflow -log logs/.nextflow.log run main.nf -profile tartu_hpc_test
+nextflow -log logs/.nextflow.log run main.nf -profile tartu_hpc_test \
+    --out_dir results.test \
+    -resume
