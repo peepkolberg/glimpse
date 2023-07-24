587e76946a6481874ee067a6aaee447a1f6a3e55
Peep Kolberg <44492022+peepkolberg@users.noreply.github.com>
2023-07-24 10:07:29 +0300

    Resolve #3 (#4)
    * Impute indels as well
    
    * Enable indel imputation
    
    * Enable indel imputation
    
    * Enable loading files from cache when resuming workflow
    
    * Calculate additional VCF tags
    
    * Filter output VCF: MAF > 1%
    
    * Minor refactor
    
    * Ignore singularity cache
    
    * Use container
    
    * Add INFO filter
    
    * Re-calculate imputation scores using ALL samples
    
    * Ignore tmp
    
    * Comment out outdir param to avoid clash with tartu_hpc_test
    
    * Delete whitespace
    
    * Specify outdir param

--- run_test.sh
+++ run_test.sh
@@ -7,9 +7,9 @@
 #SBATCH --cpus-per-task=1
 #SBATCH --mem=1G
 
-
-
 module load nextflow/22.04.3
 module load any/singularity/3.11.1
 
-nextflow -log logs/.nextflow.log run main.nf -profile tartu_hpc_test
+nextflow -log logs/.nextflow.log run main.nf -profile tartu_hpc_test \
+    --out_dir results.test \
+    -resume
